688faa52188ec254672703a21d904a3f04157ed3
Forest Tong <ftong@mit.edu>
2014-01-21 15:41:04 -0500

    Simple driver class

--- robot/src/tablet/Driver.java
+++ robot/src/tablet/Driver.java
@@ -2,24 +2,30 @@ package tablet;
 
 import comm.*;
 import devices.actuators.Cytron;
+import devices.sensors.Encoder;
 import devices.sensors.Gyroscope;
 
 public class Driver {
     private MapleComm comm;
     private Cytron leftWheel, rightWheel;
     private Gyroscope gyro;
+    private Encoder leftEncoder, rightEncoder;
     
     public Driver() {
         comm = new MapleComm(MapleIO.SerialPortType.LINUX);
         leftWheel = new Cytron(2, 1);
         rightWheel = new Cytron(7, 6);
-        gyro = new Gyroscope(1, 9);
+        //gyro = new Gyroscope(13, 9);
+        leftEncoder = new Encoder(36, 35);
+        rightEncoder = new Encoder(34, 33);
     }
     
     void setup() {
-        comm.registerDevice(leftWheel);
-        comm.registerDevice(rightWheel);
-        comm.registerDevice(gyro);
+        //comm.registerDevice(leftWheel);
+        //comm.registerDevice(rightWheel);
+        comm.registerDevice(leftEncoder);
+        comm.registerDevice(rightEncoder);
+        //comm.registerDevice(gyro);
         comm.initialize();
     }
     
@@ -31,14 +37,14 @@ public class Driver {
     
     void updateReading() {
         comm.updateSensorData();
-        System.out.println("omega: " + gyro.getOmega());
-        System.out.println("theta: " + gyro.getTheta());
+        System.out.println("left: " + leftEncoder.getTotalAngularDistance());
+        System.out.println("right: " + rightEncoder.getTotalAngularDistance());
     }
     
     void rotateToAngle(double theta) throws InterruptedException {
         double currentAngle = gyro.getTheta();
         while(Math.abs(currentAngle - theta) > 0.1) {
-            drive(-.03*(currentAngle - theta), .03*(currentAngle - theta));
+            drive(-.05*(currentAngle - theta), .05*(currentAngle - theta));
             this.updateReading();
             currentAngle = gyro.getTheta();
             Thread.sleep(100);
@@ -49,8 +55,11 @@ public class Driver {
     public static void main(String[] args) throws InterruptedException {
         Driver driver = new Driver();
         driver.setup();
-        //driver.drive(0, 0);
-        driver.rotateToAngle(1);
-        
+        //driver.drive(0.2, 0.2);
+        for(int i = 0; i < 50; i++) {
+            driver.updateReading();
+            Thread.sleep(100);
+        }
+        driver.drive(0, 0);
     }
 }
